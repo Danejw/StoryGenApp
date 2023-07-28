5c07b1b4397cc32ba94b4b82669086aaa4d9868c
Danejw <48073085+Danejw@users.noreply.github.com>
2023-07-28 08:58:50 -1000

    More nullable fixes

--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -15,22 +15,22 @@ namespace StoryGenApp.Client.Shared
     public partial class MainLayout
     {
         [Inject]
-        protected IJSRuntime JSRuntime { get; set; }
+        protected IJSRuntime? JSRuntime { get; set; }
 
         [Inject]
-        protected NavigationManager NavigationManager { get; set; }
+        protected NavigationManager? NavigationManager { get; set; }
 
         [Inject]
-        protected DialogService DialogService { get; set; }
+        protected DialogService? DialogService { get; set; }
 
         [Inject]
-        protected TooltipService TooltipService { get; set; }
+        protected TooltipService? TooltipService { get; set; }
 
         [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
+        protected ContextMenuService? ContextMenuService { get; set; }
 
         [Inject]
-        protected NotificationService NotificationService { get; set; }
+        protected NotificationService? NotificationService { get; set; }
 
         private bool sidebarExpanded = true;
 
